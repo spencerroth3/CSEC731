ee7ef573c901cfd95831f4a436308a582539f8c1
spencerroth3 <34780564+spencerroth3@users.noreply.github.com>
2022-03-16 01:31:00 -0400

    Update parser.py

--- Project2/parser.py
+++ Project2/parser.py
@@ -7,7 +7,9 @@ parser.py   This program functions as a parser for the HTTP protocol
 """
 
 import sys
-from urllib import request
+
+SUPPORTED_METHODS = {"GET", "POST", "PUT", "DELETE", "CONNECT", "HEAD"}
+SUPPORTED_VERSIONS = {"HTTP/1.0", "HTTP/1.1", "HTTP/2.0"}
 
 class RequestHTTP:
     def __init__(self, method, path, version):
@@ -15,25 +17,28 @@ class RequestHTTP:
         self.path = path
         self.version = version
         self.headers = {}
-        self.data = None
+        self.body = ""
 
 def parse_request(req):
     """
     """
     request_line = req[0].split(" ")
     req = req[1:]
-    r = RequestHTTP(request_line[0], request_line[1], request_line[2]) #.split("/")[1])
+    r = RequestHTTP(request_line[0], request_line[1], request_line[2].strip("\n")) #.split("/")[1])
 
     req_iter = req #create an iterator so we can perform destructive action on req
     for line in req_iter:
+        req = req[1:]
+        line = line.strip("\n")
         if line != "":
             h = line.split(": ")
             r.headers[h[0]] = h[1]
         else:
-            r.data += line + "\n"
-
-        req = req[1:]
-
+            break
+        
+    for line in req:
+        #print(line)
+        r.body += line
     return r
 
 
@@ -46,15 +51,25 @@ def get_request(path):
         return f.readlines()
 
 def main():
-    request_path = sys.argv[1]
-    req = get_request(request_path)
-    r = parse_request(req)
+    if len(sys.argv) < 2:
+        print("usage: parser.py <path to HTTP request>")
+        exit(1)
+    
+    try:
+        try:
+            request_path = sys.argv[1]
+            req = get_request(request_path)
+            r = parse_request(req)
 
-    print(r.headers)
-    print("-------")
-    print(r.data)
+            if r.method not in SUPPORTED_METHODS or r.version not in SUPPORTED_VERSIONS:
+                raise Exception
+            else:
+                print("200 OK")
+        except:
+            print("400 BAD REQUEST")
 
-
+    except:
+        print("500 INTERNAL SERVER ERROR")
 
 
 main()
